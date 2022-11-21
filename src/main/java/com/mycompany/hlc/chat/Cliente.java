cbb97902ce385f6c692663fe0ffdaf195e23e976
MJ-Curtido <manu17pc@gmail.com>
2022-11-21 13:30:53 +0100

    first commit

---
+++ src/main/java/com/mycompany/hlc/chat/Cliente.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.hlc.chat;
+
+/**
+ *
+ * @author Dam
+ */
+public class Cliente {
+    public static void main(String[] args) {
+        
+    }
+}
